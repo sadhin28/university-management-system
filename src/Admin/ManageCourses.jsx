6f547fb4315e77a8a1af1b343269f91d21670f3d
sadhin28 <taosifsadhin113@gmail.com>
2025-09-07 02:42:39 +0600

    add admin route

---
+++ src/Admin/ManageCourses.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageCourses = () => {
+    return (
+        <div>
+            <h1>Manage courses</h1>
+        </div>
+    );
+};
+
+export default ManageCourses;
