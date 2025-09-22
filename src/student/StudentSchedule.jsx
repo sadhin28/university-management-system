5b0b3d90436e444fbb0d52196df314d57fe14429
sadhin28 <taosifsadhin113@gmail.com>
2025-09-22 19:30:08 +0600

    add button

--- src/student/StudentSchedule.jsx
+++ src/student/StudentSchedule.jsx
@@ -221,7 +221,7 @@ export default function SchedulePage() {
             </div>
             {(role === "admin" || role === "teacher") && (
               <div className="flex gap-2">
-                <button onClick={() => handleUpdate(item)} className="bg-yellow-500 text-white px-3 py-1 rounded-lg hover:bg-yellow-600">✏️ Update</button>
+                <button onClick={() => handleUpdate(item)} className="bg-green-500 text-white px-3 py-1 rounded-lg hover:bg-yellow-600">✏️ Update</button>
                 <button onClick={() => handleDelete(item._id, item.course)} className="bg-red-500 text-white px-3 py-1 rounded-lg hover:bg-red-600">🗑 Delete</button>
               </div>
             )}
