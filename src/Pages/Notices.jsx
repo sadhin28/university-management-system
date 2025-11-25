863eee20f17783410f435182f2e20a2dce022e9d
sadhin28 <taosifsadhin113@gmail.com>
2025-11-26 01:48:04 +0600

    update notice section

--- src/Pages/Notices.jsx
+++ src/Pages/Notices.jsx
@@ -80,7 +80,7 @@ export default function Notices() {
   };
 
   return (
-    <div className="max-w-6xl mx-auto p-6">
+    <div className="max-w-7xl mx-auto p-6">
       <h2 className="text-2xl font-bold text-center mb-6">📢 University Notice Board</h2>
 
       {(role === "admin" || role === "teacher") && (
