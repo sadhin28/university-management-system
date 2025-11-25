3b97a20520032798f7d27ffc4eb51536743703e8
sadhin28 <taosifsadhin113@gmail.com>
2025-11-26 02:48:15 +0600

    add bell icon

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { FaBell } from "react-icons/fa";
+import {  FaRegBell } from "react-icons/fa";
 import { AuthContext } from "../Provider/Authprovider";
 import { Link } from "react-router-dom";
 
@@ -23,7 +23,7 @@ const Navbar = () => {
         )}
       </div></Link>:<Link to="/notices">
             <div className="relative">
-        <FaBell className="text-2xl text-blue-800  cursor-pointer" />
+        <FaRegBell className="text-2xl text-white  cursor-pointer" />
         {unseenCount > 0 && (
           <span className="absolute -top-1 -right-1 bg-red-600 text-white rounded-full w-4 h-4 text-xs flex items-center justify-center">
             {unseenCount}
