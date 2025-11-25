7695e6ee25492e8487942bcf7c02b85fc1ea2f8f
sadhin28 <taosifsadhin113@gmail.com>
2025-11-26 02:06:01 +0600

    update all

--- src/Admin/Payment.jsx
+++ src/Admin/Payment.jsx
@@ -18,14 +18,20 @@ export default function Payment() {
 
   return (
     <div className="max-w-8xl mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-4 text-center">
+      <h1 className="text-2xl text-white font-bold mb-4 text-center">
         Student Payment Transactions
       </h1>
+      {/* Total Paid */}
+      <div className="mt-4 my-4 text-left text-white">
+        <h2 className="font-semibold text-lg">
+          Total Paid: {totalPaid} BDT
+        </h2>
+      </div>
       {/* Transactions Table */}
       <div className="overflow-x-auto">
         <table className="table-auto border-collapse border border-gray-400 w-full">
           <thead>
-            <tr className="bg-gray-200">
+            <tr className=" bg-gradient-to-b from-[#1D5A5AFF] to-[#031226FF] to-[#0881B5FF] text-white">
               <th className="border px-4 py-2">Transaction ID</th>
               <th className="border px-4 py-2">Date</th>
               <th className="border px-4 py-2">Amount</th>
@@ -36,7 +42,7 @@ export default function Payment() {
           <tbody>
             {payments.map((pay, idx) => (
               
-              <tr key={idx}>
+              <tr className="hover:bg-blue-300" key={idx}>
                 <td className="border px-4 py-2">{pay.id}</td>
                 <td className="border px-4 py-2">{pay.date}</td>
                 <td className="border px-4 py-2">{pay.amount} BDT</td>
@@ -47,12 +53,7 @@ export default function Payment() {
         </table>
       </div>
 
-      {/* Total Paid */}
-      <div className="mt-4 text-center">
-        <h2 className="font-semibold text-lg">
-          Total Paid: {totalPaid} BDT
-        </h2>
-      </div>
+      
     </div>
   );
 }
